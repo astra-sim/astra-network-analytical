32f8c9e251d4995531558c4f9899f3998468f06f
William Won <william.won@gatech.edu>
2020-10-12 15:52:51 -0400

    Implement Torus2D routing

--- src/topology/Torus2D.hh
+++ src/topology/Torus2D.hh
@@ -31,15 +31,60 @@ namespace Analytical {
     public:
         using TopologyConfigurations = TopologyConfiguration::TopologyConfigurations;
 
+        /**
+         * Construct a Torus2D topology.
+         * Caveat: currently only support square one.
+         *
+         * @param configurations configuration for each dimension
+         * @param npus_count total number of npus connected to this torus2D
+         */
         Torus2D(const TopologyConfigurations& configurations, int npus_count) noexcept;
 
         Latency send(NpuId src_id, NpuId dest_id, PayloadSize payload_size) noexcept override;
+
     private:
+        using Direction = int;  // +1 for up/right, -1 for down/left
+
         NpuAddress npuIdToAddress(NpuId id) const noexcept override;
         NpuId npuAddressToId(const NpuAddress& address) const noexcept override;
 
-        int width;
-        int half_width;
+        int width;  // width of the torus, therefore (npus_count = width * width)
+        int half_width;  // 1/2 of the width, used for choosing direction
+
+        /**
+         * Compute which direction the index should move.
+         *
+         * @param src_index
+         * @param dest_index
+         * @return +1 if next_index = current_index + 1
+         *         -1 if next_index = current_index - 1
+         */
+        Direction computeDirection(NpuId src_index, NpuId dest_index) const noexcept;
+
+        /**
+         * Translate npuId to row-col pair.
+         * (In the code, each row-col coordinate is denoted as 'index')
+         * @param id npuId
+         * @return (row, col) tuple
+         */
+        std::pair<int, int> idToRowCol(NpuId id) const noexcept;
+
+        /**
+         * Translate (row, col) to NPU's ID.
+         * @param row
+         * @param column
+         * @return npuId
+         */
+        NpuId rowColToId(int row, int column) const noexcept;
+
+        /**
+         * Compute the next index after taking a single step towards direction.
+         *
+         * @param current_index
+         * @param direction direction to move
+         * @return next_index after taking a step
+         */
+        int takeStep(int current_index, Direction direction) const noexcept;
     };
 }
 
