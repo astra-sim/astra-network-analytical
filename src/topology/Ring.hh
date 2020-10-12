f00ed3d07642438d7333a2ef03e87b9f0cbd27fa
William Won <william.won@gatech.edu>
2020-10-12 15:07:10 -0400

    Update Ring comment

--- src/topology/Ring.hh
+++ src/topology/Ring.hh
@@ -29,6 +29,12 @@ Author : William Won (william.won@gatech.edu)
 namespace Analytical {
     class Ring : public Topology {
     public:
+        /**
+         * Construct new Ring topology.
+         * @param configurations configurations per each dimension
+         * @param npus_count number of npus connected to a ring.
+         * @param bidirectional set this to true if the ring is bidirectional.
+         */
         Ring(const TopologyConfigurations& configurations, int npus_count, bool bidirectional=true) noexcept;
 
         Latency send(NpuId src_id, NpuId dest_id, PayloadSize payload_size) noexcept override;
@@ -39,10 +45,26 @@ namespace Analytical {
         NpuId npuAddressToId(const NpuAddress& address) const noexcept override;
 
         int npus_count;
-        int half_npus_count;
+        int half_npus_count;  // Half of the number of NPUs connected to this ring: used when computing direction
         bool bidirectional;  // whether ring is bidirectional or unidirectional
 
+        /**
+         * Compute which direction a packet should proceed.
+         *
+         * @param src_id
+         * @param dest_id
+         * @return +1 if a packet should move to (currentId + 1)
+         *         -1 if a packet should move to (currentId - 1)
+         */
         Direction computeDirection(NpuId src_id, NpuId dest_id) const noexcept;
+
+        /**
+         * Take a step towards the given direction and return the destined node after taking single step.
+         *
+         * @param current_id
+         * @param direction direction to take a step
+         * @return npuId after taking a step
+         */
         NpuId takeStep(NpuId current_id, Direction direction) const noexcept;
     };
 }
