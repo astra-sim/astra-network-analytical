a21339568d8a14289f075cbaf94d406b8b1ef766
William Won <william.won@gatech.edu>
2020-11-08 10:15:45 -0500

    Add DetailedTorus2D topology

---
+++ src/topology/detailed/DetailedTorus2D.hh
@@ -0,0 +1,34 @@
+/******************************************************************************
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*******************************************************************************/
+
+#ifndef __DETAILEDRING_HH__
+#define __DETAILEDRING_HH__
+
+#include "../DetailedTopology.hh"
+
+namespace Analytical {
+    class DetailedTorus2D : public DetailedTopology {
+    public:
+        DetailedTorus2D(TopologyConfigs configs) noexcept;
+
+        ~DetailedTorus2D() noexcept override;
+
+        double send(NpuId src, NpuId dest, PayloadSize payload_size) noexcept override;
+
+    private:
+        using Direction = int;
+
+        int width;
+        int height;
+
+        NpuAddress npuIdToAddress(NpuId npu_id) const noexcept override;
+        NpuId npuAddressToId(NpuAddress npu_address) const noexcept override;
+
+        Direction computeDirection(NpuId src, NpuId dest, int ring_size);
+        NpuId takeStep(NpuId current_node, Direction direction, int ring_size);
+    };
+}
+
+#endif
