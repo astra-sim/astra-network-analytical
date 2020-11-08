a4c1010050c9c47e4a9ae2a81c040b992fd4b428
William Won <william.won@gatech.edu>
2020-11-08 09:43:38 -0500

    Add DetailedSwitch topology

---
+++ src/topology/detailed/DetailedSwitch.hh
@@ -0,0 +1,25 @@
+/******************************************************************************
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*******************************************************************************/
+
+#ifndef __DETAILEDSWITCH_HH__
+#define __DETAILEDSWITCH_HH__
+
+#include "../DetailedTopology.hh"
+
+namespace Analytical {
+    class DetailedSwitch : public DetailedTopology {
+    public:
+        DetailedSwitch(TopologyConfigs configs) noexcept;
+
+        ~DetailedSwitch() noexcept override;
+
+        double send(NpuId src, NpuId dest, PayloadSize payload_size) noexcept override;
+
+    private:
+        NpuId switch_id;
+    };
+}
+
+#endif
