afb886adc8b9a668425e2c3dbf3ca712b20bdd46
Will Won <william.won@gatech.edu>
2024-03-02 03:35:19 -0500

    Implement algorithm to compute all basic topology connectivity
    Signed-off-by: Will Won <william.won@gatech.edu>

--- congestion_aware/multi-dim-topology/MultiDimTopology.cc
+++ congestion_aware/multi-dim-topology/MultiDimTopology.cc
@@ -52,20 +52,85 @@ Route MultiDimTopology::route(const DeviceId src, const DeviceId dest)
 void MultiDimTopology::construct_topology() noexcept {
   // compute the number of NPUs
   npus_count = 1;
-  for (const auto npus_count_dim : npus_count_per_dim) {
-    npus_count *= npus_count_dim;
+  for (const auto basic_topology_npus_count : npus_count_per_dim) {
+    npus_count *= basic_topology_npus_count;
   }
 
   // instantiate devices
   instantiate_devices();
 
+  // initialize map storing intra/inter-group strides and repetitions count
+  auto basic_topology_stride = 1;
+  auto basic_topologies_count = 1;
+  auto group_stride = 1;
+  auto groups_count = npus_count;
+
   // construct dimensions
   for (auto dim = 0; dim < dims_count; dim++) {
-    construct_dimension(dim);
+    construct_dimension(
+        dim,
+        &basic_topology_stride,
+        &basic_topologies_count,
+        &group_stride,
+        &groups_count);
   }
 }
 
-void MultiDimTopology::construct_dimension(const int dim) noexcept {
-  // TODO: implement dimension construction
-  std::cout << "construct dim " << dim << std::endl;
+void MultiDimTopology::construct_dimension(
+    const int dim,
+    int* const basic_topology_stride,
+    int* const basic_topologies_count,
+    int* const group_stride,
+    int* const groups_count) noexcept {
+  // get the number of NPUs for each basic topology we're constructing
+  const auto basic_topology_npus_count = npus_count_per_dim[dim];
+
+  // start from base index 0
+  auto group_base_idx = 0;
+  auto basic_topology_base_idx = 0;
+
+  // update group stride/repetition before processing
+  *group_stride *= basic_topology_npus_count;
+  *groups_count /= basic_topology_npus_count;
+
+  // there are multiple "topology groups" within a multi-dim topology
+  for (auto group_iter = 0; group_iter < *groups_count; group_iter++) {
+    // here, a "topology group" is being constructed
+
+    // first, initialize basic_topology_base_idx
+    // which will be used for basic topologies within a group
+    basic_topology_base_idx = group_base_idx;
+
+    // each "topology group" has multiple "basic topologies"
+    for (auto basic_topology_iter = 0;
+         basic_topology_iter < *basic_topologies_count;
+         basic_topology_iter++) {
+      // here, a "basic topology" is being constructed
+
+      // first compute the list of NPUs within this basic topology
+      auto basic_topology_npus = std::vector<int>();
+
+      // which has <basic_topology_npus_count> number of NPUs
+      // starting from basic_topology_base_idx
+      auto topology_npu_idx = basic_topology_base_idx;
+
+      // append all NPUs within this basic topology
+      for (auto basic_topo_npus_iter = 0;
+           basic_topo_npus_iter < basic_topology_npus_count;
+           basic_topo_npus_iter++) {
+        basic_topology_npus.push_back(topology_npu_idx);
+        topology_npu_idx += *basic_topology_stride;
+      }
+
+      // process next basic topology
+      basic_topology_base_idx++;
+    }
+
+    // process next topology group
+    group_base_idx += *group_stride;
+  }
+
+  // update basic topology stride/repetition after processing
+  *basic_topology_stride *= basic_topology_npus_count;
+  *basic_topologies_count *= basic_topology_npus_count;
 }
--- include/astra-network-analytical/congestion_aware/MultiDimTopology.hh
+++ include/astra-network-analytical/congestion_aware/MultiDimTopology.hh
@@ -31,7 +31,8 @@ class MultiDimTopology : public Topology {
   /**
    * Construct the multi-dimensional network topology
    * including the instantiation of devices
-   * and constructing each dimension's connectivity.
+   * and constructing each dimension's connectivity
+   * using the construct_dimension() method iteratively across all dimensions.
    */
   void construct_topology() noexcept;
 
@@ -39,9 +40,83 @@ class MultiDimTopology : public Topology {
    * Construct dim i of the multi-dimensional network
    * which creates multiple basic building blocks across devices.
    *
-   * @param dim network dimension to construct
+   * e.g., let's say we have a 4x3x2 (=24) topology
+   * (1) Constructing the 1st dim: we should create 3x2 = 6 basic topologies
+   *
+   *    [ 0 1 2 3 ]       Group 0
+   *    --------------------------
+   *    [ 4 5 6 7 ]       Group 1
+   *    ---------------------------
+   *    [ 8 9 10 11 ]     Group 2
+   *    ---------------------------
+   *    [ 12 13 14 15 ]   Group 3
+   *    ---------------------------
+   *    [ 16 17 18 19 ]   Group 4
+   *    ---------------------------
+   *    [ 20 21 22 23 ]   Group 5
+   *
+   *    -> We have 24/4 = 6 groups, group stride = 4
+   *    -> Within each group: 1 basic topology, basic topology stride = 1
+   *
+   * (2) Constructing the 2nd dim: we should create 4x2 = 8 basic topologies
+   *
+   *    [ 0 4 8 ]         Group 0
+   *    [ 1 5 9 ]           - Group Base idx: 0
+   *    [ 2 6 10 ]          - Within a group, 4 basic topologies
+   *    [ 3 7 11 ]              Whose stride is 4.
+   *    ---------------------------------------------------------
+   *    [ 12 16 20 ]       Group 1
+   *    [ 13 17 21 ]        - Group Base idx: 12
+   *    [ 14 18 22 ]
+   *    [ 15 19 23 ]
+   *
+   *    -> We have 6/3 = 2 groups, group stride = 4*3 = 12
+   *    -> Within each group: 1*4 = 4 basic topology,
+   *                          basic topology stride = 1*4 = 4
+   *
+   * (3) Constructing the 3rd dim: we should create 4x3 = 12 basic topologies
+   *     [ 0 12 ]
+   *     [ 1 13 ]
+   *     [ 2 14 ]
+   *     [ 3 15 ]          All in Group 0
+   *     [ 4 16 ]
+   *     [ 5 17 ]
+   *     ...
+   *     [ 10 22 ]
+   *     [ 11 23 ]
+   *     -------------------------------------------
+   *
+   *    -> We have 2/2 = 1 groups, group stride = 12*2 = 24 (don't care)
+   *    -> Within each group: 4*3 = 12 basic topology,
+   *                          basic topology stride = 4*3 = 12
+   *
+   * We therefore set
+   *    <initial>
+   *        basic_topology_stride = 1
+   *        basic_topologies_count = 1
+   *        group_stride = 1
+   *        groups_count = npus_count
+   *    <update before processing>
+   *        group_stride *= npus_count[dim]
+   *        groups_count /= npus_count[dim]
+   *    <update after processing>
+   *        basic_topology_stride *= npus_count[dim]
+   *        basic_topologies_count *= npus_count[dim]
+   *
+   * Which are the function parameters below.
+   *
+   * @param dim dimension to construct
+   * @param basic_topology_stride stride info
+   * @param basic_topologies_count repetition info
+   * @param group_stride stride info
+   * @param groups_count repetition info
    */
-  void construct_dimension(int dim) noexcept;
+  void construct_dimension(
+      int dim,
+      int* basic_topology_stride,
+      int* basic_topologies_count,
+      int* group_stride,
+      int* groups_count) noexcept;
 };
 
 } // namespace NetworkAnalyticalCongestionAware
